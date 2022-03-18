f4b8827b481044ff59d831288841ac1f7be3c3c5
Eugene <64761635+Adequm@users.noreply.github.com>
2022-03-19 02:13:27 +0300

    dist payload

---
+++ precache-manifest.568de6e9ebc56c656759b74d1419d56f.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4dca0daff9e18caee9",
+    "url": "/minis-wishes/css/app.713caf7a.css"
+  },
+  {
+    "revision": "d0e41a7b586d571c9a1b",
+    "url": "/minis-wishes/css/chunk-vendors.adace9a3.css"
+  },
+  {
+    "revision": "706b86fa881ef644c6af4b9b66acdc98",
+    "url": "/minis-wishes/fonts/uicons-regular-straight.706b86fa.woff"
+  },
+  {
+    "revision": "92fb69a78d45a096e792b703b411150e",
+    "url": "/minis-wishes/fonts/uicons-regular-straight.92fb69a7.woff2"
+  },
+  {
+    "revision": "b1eab1a13c7e89c167bd546c7b5bc90a",
+    "url": "/minis-wishes/fonts/uicons-regular-straight.b1eab1a1.eot"
+  },
+  {
+    "revision": "639c619af28e6a1917b317bb5679f870",
+    "url": "/minis-wishes/icon.svg"
+  },
+  {
+    "revision": "289ec0291abfd8bbc423915fe43859ff",
+    "url": "/minis-wishes/icons/apple.png"
+  },
+  {
+    "revision": "1897d4ff613049a782d732d165d7dfa9",
+    "url": "/minis-wishes/icons/avocado.png"
+  },
+  {
+    "revision": "d8f1ec39bd9dbe2c96094aff8d7cf351",
+    "url": "/minis-wishes/icons/backpack.png"
+  },
+  {
+    "revision": "e25c97768398ea5305e1b256cc160ecd",
+    "url": "/minis-wishes/icons/banana.png"
+  },
+  {
+    "revision": "ebd958a94801edbed5406122da844206",
+    "url": "/minis-wishes/icons/banknote.png"
+  },
+  {
+    "revision": "7f210d0b37384715746ce7dfd88d0937",
+    "url": "/minis-wishes/icons/beer.png"
+  },
+  {
+    "revision": "155e34c09e1772026b6d7b535fba6eca",
+    "url": "/minis-wishes/icons/biscuit.png"
+  },
+  {
+    "revision": "937f38fe84b18c29dea41d0f91ee24b5",
+    "url": "/minis-wishes/icons/bong.png"
+  },
+  {
+    "revision": "5253002fbc7a636d2b6661fa792d7bf6",
+    "url": "/minis-wishes/icons/broccoli.png"
+  },
+  {
+    "revision": "96a7addf189e109eaa1d06ca5a85fc78",
+    "url": "/minis-wishes/icons/burrito.png"
+  },
+  {
+    "revision": "e309c8401a9ab94f5392262ea52f5ba0",
+    "url": "/minis-wishes/icons/cake.png"
+  },
+  {
+    "revision": "e3454413886a8c4bb76f45a66703e5b3",
+    "url": "/minis-wishes/icons/chicken.png"
+  },
+  {
+    "revision": "1eceab3594a18af5f4d5e8b6c3616ed5",
+    "url": "/minis-wishes/icons/clown.png"
+  },
+  {
+    "revision": "f5d5aeae0ed99652bcb40a7ea3fac619",
+    "url": "/minis-wishes/icons/coconut.png"
+  },
+  {
+    "revision": "f472076d3f93ca2dd5a275b384aafff2",
+    "url": "/minis-wishes/icons/coffee.png"
+  },
+  {
+    "revision": "4271bf060df868baa5dd368f2bbbbb83",
+    "url": "/minis-wishes/icons/corn.png"
+  },
+  {
+    "revision": "8e9ddbc0cb78d5f1936fbce8de38069a",
+    "url": "/minis-wishes/icons/crown.png"
+  },
+  {
+    "revision": "c6eab98830040846c5ebfff7914c47f2",
+    "url": "/minis-wishes/icons/donut.png"
+  },
+  {
+    "revision": "6203a1f764c939623e18e6b3086bb0da",
+    "url": "/minis-wishes/icons/duck.png"
+  },
+  {
+    "revision": "c883fa4f26747bf1777f55d0e16b79e3",
+    "url": "/minis-wishes/icons/dumplings.png"
+  },
+  {
+    "revision": "c9e9be8bdd122c6e615a32ec44a97327",
+    "url": "/minis-wishes/icons/egg.png"
+  },
+  {
+    "revision": "5dfb6d0d0eac8387472ac890db2cc8a7",
+    "url": "/minis-wishes/icons/energy.png"
+  },
+  {
+    "revision": "15291bd05d4b65680e2df361a2676dde",
+    "url": "/minis-wishes/icons/fish.png"
+  },
+  {
+    "revision": "d33863415816ad5471ad5e5d39ba0739",
+    "url": "/minis-wishes/icons/guitar.png"
+  },
+  {
+    "revision": "b8d9a98822892443cb9649b91afc9754",
+    "url": "/minis-wishes/icons/guitar2.png"
+  },
+  {
+    "revision": "9b47b6bfbdf7a550919a7c1a59f2af9a",
+    "url": "/minis-wishes/icons/honey.png"
+  },
+  {
+    "revision": "2e8d561f9835da8a30ac77d7271c50d9",
+    "url": "/minis-wishes/icons/horse.png"
+  },
+  {
+    "revision": "7f857628d75a65ca16c827ee6d206584",
+    "url": "/minis-wishes/icons/kiwi.png"
+  },
+  {
+    "revision": "b2825539c1d4c3f6021400859c399735",
+    "url": "/minis-wishes/icons/lemon.png"
+  },
+  {
+    "revision": "e3786ccda26a83fd5caf0397abebffa6",
+    "url": "/minis-wishes/icons/marijuana.png"
+  },
+  {
+    "revision": "39dd9635c61aa5866de8892e406fb139",
+    "url": "/minis-wishes/icons/medicine.png"
+  },
+  {
+    "revision": "3d88d897a7e66b0c09e1d76f3b0bbf3b",
+    "url": "/minis-wishes/icons/orange.png"
+  },
+  {
+    "revision": "3c49e77b4efa5dbfc0950fc5e8ad161b",
+    "url": "/minis-wishes/icons/pizza.png"
+  },
+  {
+    "revision": "405ed64365044f5c9195cdb8096e7dcc",
+    "url": "/minis-wishes/icons/plant.png"
+  },
+  {
+    "revision": "cd7e16c72296dd08bb1016449018e59b",
+    "url": "/minis-wishes/icons/prawn.png"
+  },
+  {
+    "revision": "e492b22b5f654a0501f36397176ba7a9",
+    "url": "/minis-wishes/icons/robot.png"
+  },
+  {
+    "revision": "11c23cc554ad30de74af845bbbe24ced",
+    "url": "/minis-wishes/icons/seal.png"
+  },
+  {
+    "revision": "a16f5e37efdd12832549094baad770da",
+    "url": "/minis-wishes/icons/shamrock.png"
+  },
+  {
+    "revision": "afd91fc0074b9bdaede78c3a97407639",
+    "url": "/minis-wishes/icons/shrimp.png"
+  },
+  {
+    "revision": "02c015beb6739b5669be72d1cbf7a1bc",
+    "url": "/minis-wishes/icons/strawberry.png"
+  },
+  {
+    "revision": "665507598330be198983b5a43e675373",
+    "url": "/minis-wishes/icons/tea.png"
+  },
+  {
+    "revision": "5164d4945a1e9701c97ec8a372a30193",
+    "url": "/minis-wishes/icons/tulip.png"
+  },
+  {
+    "revision": "9c3363b6f193521d3bd8f28376814e6b",
+    "url": "/minis-wishes/icons/unicorn.png"
+  },
+  {
+    "revision": "589ef1443a54e3fcd90fa0edc2b8a40e",
+    "url": "/minis-wishes/icons/watermelon.png"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/minis-wishes/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "7f3b41902b1de1e9c74769f582e16c9d",
+    "url": "/minis-wishes/index.html"
+  },
+  {
+    "revision": "1f4dca0daff9e18caee9",
+    "url": "/minis-wishes/js/app.ffd0c783.js"
+  },
+  {
+    "revision": "d0e41a7b586d571c9a1b",
+    "url": "/minis-wishes/js/chunk-vendors.3005a015.js"
+  },
+  {
+    "revision": "ed1d0a1bbbd436d0885b19b360ee9723",
+    "url": "/minis-wishes/manifest.json"
+  },
+  {
+    "revision": "6870cb641a99793d39df3f89ce00de74",
+    "url": "/minis-wishes/wishes.ru.json"
+  }
+]);
